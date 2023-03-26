3b182abd691f9abaf08f5dfd4f82cfe55e1f0973
Connor M <126525684+cjmcg3@users.noreply.github.com>
2023-03-25 21:41:58 -0400

    Update thermo.c

--- thermo.c
+++ thermo.c
@@ -1,4 +1,5 @@
 /*Connor MCGarry --------- 03/20/23 --------- ECE231 lab2
+// Reuploaded with added code for more stable display temps and a temperature limit
 
 The following code recieves data from a TMP36 temperature sensor on ADC5 of an arduino, and displays the temp in F on a four digit, 7 segment display.
 The value changes from F to C when a button is being pressed, and the output is contantly output to a serial moniter via USART*/
@@ -7,7 +8,7 @@ The value changes from F to C when a button is being pressed, and the output is 
 #include <util/delay.h>
 #include <string.h>
 #include <stdlib.h>
-#define MYDELAY 2
+#define MYDELAY 1
  
 
 void uart_init(void);
@@ -18,26 +19,42 @@ unsigned int get_adc(void);
 
 int main(void){
     
-    unsigned int digitalValue;
+    unsigned int digitalValue,avg;
     int tempC, tempF;
     adc_init();
     uart_init();
-    PORTD = 1<<PORTD2;
     unsigned char ledDigits[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D,
     0x07, 0x7F, 0x67};  // NUmber values
-    unsigned char deci, ones, tens, unit;
-    unsigned char test[6];
-    unsigned char test2[6];
+    unsigned char deci, ones, tens, unit, count;
+    PORTD = 1<<PORTD2;
     DDRC = 0x0F;  // PC0 - PC3
     DDRD = 0xF0;  //PD4 - PD7
     DDRB = 0xFF;   // Digit enable pins (9,10,11,12)
-
-        
+    int i;
+    count = 1;
         
     while(1){
-        digitalValue = get_adc();  
+        
+// -------------------------------------------------------------------    
+    // Temperature calculations for average/ensurig more stable display
+        avg = 0;
+        for (i=0; i<20; i++){
+            avg = avg + get_adc();
+        }
+
+        digitalValue = avg / 20;
+        if (count % 20 == 1){
         tempC = (digitalValue * 1.1/10.24 - 50.0)*10;
         tempF = (tempC * 9.0/5.0/10 + 32.0)*10;
+        if (tempF>999){
+            tempF = 999;   // Don't let the temperature go over 99.9 as it cannot be displayed
+        }
+        count = 1;
+        }
+        count++;
+
+// ------------------------------------------------------------------------
+
 
         if ((PIND&(1<<PIND2))==0){ // If the button is being pressed
             tens = (tempC/100)%10;
@@ -50,6 +67,9 @@ int main(void){
             uart_send('.');
             uart_send(deci+'0');
             uart_send('C');
+            uart_send(13);
+            uart_send(10);
+            _delay_ms(2);  
             }
 
         else{
@@ -63,8 +83,12 @@ int main(void){
             uart_send('.');
             uart_send(deci+'0');
             uart_send('F');
+            uart_send(13);
+            uart_send(10);
+            _delay_ms(2);  
         }
 
+        
         PORTC = (PORTC&0xF0)|(unit&0x0F);  // Chooses either the C or F value 
         PORTD = (unit&0xF0)|(PORTD&0x0F);
         PORTB = ~ (1<<1);   // Enables digit
@@ -85,11 +109,8 @@ int main(void){
         PORTB = ~ (1<<4);
         _delay_ms(MYDELAY);
 
-        PORTB = 0xFF;
+        PORTB = 0xFF;  // keeps digits on
 
-        uart_send(13);
-        uart_send(10);
-        _delay_ms(2);  
     }
     return 0;
 }
